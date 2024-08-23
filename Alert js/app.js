95f010122d93ad1ed8dcd7b7f50f2a903f852340
Hamza Shaikh <2hamzashaikh@gmail.com>
2024-08-23 12:15:29 +0500

    Add files via upload

---
+++ Alert js/app.js
@@ -0,0 +1,23 @@
+alert("Error! Please Enter a Valid Password..")
+alert("Welcome To JS Land .. \n Happy Coding !")
+alert("Welcome To JS Land ..")
+alert("Hello.. I can Run JS through My web Browser Console ");
+      
+var msg = "Hello World !"
+var uname = "Hamza Shaikh"
+var age = "12 Years"
+var course = "Certified Mobile Application Development"
+var food = "Pizza \n Pizz \n Piz \n Pi \n P"
+var email = "My Email Address is example@example.com"
+var book = "I am trying to learn from the Book. \nA smarter way to learn Javascript"
+var symbol = "--------------- ║ ︵ 〄 ︵ ║ ---------------"
+
+
+alert(msg)
+alert(uname)
+alert(age)
+alert(course)
+alert(food)
+alert(email)
+alert(book)
+alert(symbol)
